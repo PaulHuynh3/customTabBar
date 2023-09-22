c949d3978850daa419ba7786a09b34cbf24913e5
Paul <paul.huynh3@gmail.com>
2023-09-22 11:12:50 -0400

    Build custom tabbar with system images interacting with button

--- CustomTabBar/ContentView.swift
+++ CustomTabBar/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedIndex = 0
+    
+    let tabBarImageNames = ["person", "gear", "plus.app.fill", "pencil", "lasso"]
+
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            ZStack {
+                switch selectedIndex {
+                case 0:
+                    NavigationView {
+                        Text("first")
+                            .navigationTitle("First tab")
+                    }
+                default:
+                    Text("Rest")
+                }
+            }
+            
+            Spacer()
+            
+            HStack {
+                ForEach(0..<5) { num in
+                    Spacer()
+                    Button {
+                        selectedIndex = num
+                    } label: {
+                        Image(systemName: tabBarImageNames[num])
+                            .font(.system(size: 25, weight: .bold))
+                            .foregroundColor(Color(.black))
+                    }
+                    Spacer()
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
