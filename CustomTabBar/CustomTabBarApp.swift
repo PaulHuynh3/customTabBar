a2fc2c4d74c6bafceb50bd63329485c000185ff2
Paul <paul.huynh3@gmail.com>
2023-09-22 10:16:19 -0400

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Paul Huynh on 2023-09-22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
